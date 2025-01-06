d4dcf302c4a4e6b60e652f16f2c4c35246050ed5
Miko <mikolaj.musialik.media@gmail.com>
2025-01-06 12:14:09 +0100

    Interpolation (C14)

--- OpenGLApp/main.cpp
+++ OpenGLApp/main.cpp
@@ -36,22 +36,27 @@ static const char* vShader = "                                             \n\
                                                                            \n\
 layout (location = 0) in vec3 pos;                                         \n\
                                                                            \n\
+out vec4 vCol;                                                             \n\
+                                                                           \n\
 uniform mat4 model;                                                        \n\
                                                                            \n\
 void main()                                                                \n\
 {                                                                          \n\
-         gl_Position = model * vec4(pos, 1.0);  \n\
+         gl_Position = model * vec4(pos, 1.0);                             \n\
+         vCol = vec4(clamp(pos, 0.0f, 1.0f), 1.0f);                        \n\
 }";    
 
 //Fragment Shader
 static const char* fShader = "                                     \n\
 #version 330                                                       \n\
                                                                    \n\
+in vec4 vCol;                                                      \n\
+                                                                   \n\
 out vec4 color;                                                    \n\
                                                                    \n\
 void main()                                                        \n\
 {                                                                  \n\
-         color = vec4(1.0, 0.0, 0.0, 1.0);                         \n\
+         color = vCol;                                             \n\
 }";
 
 
@@ -225,9 +230,10 @@ int main()
         glUseProgram(shader);
 
         glm::mat4 model(1.0f);
-        model = glm::rotate(model, curAngle * toRadians, glm::vec3(0.0f, 0.0f, 1.0f)); //Rotate Model
-        model = glm::translate(model, glm::vec3(triOffset, 0.0f, 0.0f)); //Move Model
-        model = glm::scale(model, glm::vec3(curSize, curSize, 0.0f)); //Scale Model
+        //model = glm::rotate(model, curAngle * toRadians, glm::vec3(0.0f, 0.0f, 1.0f)); //Rotate Model
+        //model = glm::translate(model, glm::vec3(triOffset, 0.0f, 0.0f)); //Move Model
+        //model = glm::scale(model, glm::vec3(curSize, curSize, 0.0f)); //Scale Model
+        model = glm::scale(model, glm::vec3(0.4f, 0.4f, 0.0f)); //Scale Model
 
         glUniformMatrix4fv(uniformMove, 1, GL_FALSE, glm::value_ptr(model));
 
